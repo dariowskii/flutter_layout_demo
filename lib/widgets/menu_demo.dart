c06c9bd625fbcee939d1ef4379052d2a4f872e44
Dario Varriale <dario.varriale@atobit.it>
2023-01-28 15:44:03 +0100

    Update drawer

--- lib/widgets/menu_demo.dart
+++ lib/widgets/menu_demo.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 class MenuDemo extends StatelessWidget {
   const MenuDemo({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -14,9 +14,13 @@ class MenuDemo extends StatelessWidget {
           height: 100,
           decoration: BoxDecoration(
             color: Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(10),
-            ),
+            borderRadius: size.width >= 1024
+                ? const BorderRadius.vertical(
+                    top: Radius.circular(10),
+                  )
+                : const BorderRadius.only(
+                    topRight: Radius.circular(10),
+                  ),
           ),
           child: const Center(
             child: Text(
